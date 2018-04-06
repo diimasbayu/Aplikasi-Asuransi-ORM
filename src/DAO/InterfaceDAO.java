95285d99279a23ffb838beeb167a1a5ab03a06c4
dimas bayu gumelar <dbayu15@gmail.com>
2018-04-06 10:06:42 +0700

    step1

---
+++ src/DAO/InterfaceDAO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.util.List;
+
+/**
+ *
+ * @author dbayu
+ */
+public interface InterfaceDAO {
+    public boolean insert(Object object);
+    public boolean update(Object object);
+    public boolean delete(Object object);
+    public List<Object> getAll();
+    public List<Object> search(String category, String search);
+    public Object getById(String id);
+    
+}
