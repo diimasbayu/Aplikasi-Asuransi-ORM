478855b213f9478a236dc57241791e28030f0020
Purbarini <rinisulysthian@gmail.com>
2018-04-06 11:05:04 +0700

    update rini nasabah admin DAO sama View

--- src/DAO/AdminDAO.java
+++ src/DAO/AdminDAO.java
@@ -5,10 +5,59 @@
  */
 package DAO;
 
+import entitites.Admin;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import tools.HibernateUtil;
+
 /**
  *
  * @author dbayu
  */
-public class AdminDAO {
+
+public class AdminDAO implements InterfaceDAO {
+
+    public Session session;
+    private SessionFactory factory;
+    public Transaction transaction;
+
+    public FunctionDAO fdao;
+
+    public AdminDAO() {
+        this.fdao = new FunctionDAO(HibernateUtil.getSessionFactory());
+    }
+
+    @Override
+    public boolean insert(Object object) {
+         return fdao.insert(object);
+    }
+
+    @Override
+    public boolean update(Object object) {
+        return fdao.insert(object);
+    }
+
+    @Override
+    public boolean delete(Object object) {
+         return fdao.delete(Admin.class, object +"");
+    }
+
+    @Override
+    public List<Object> getAll() {
+        return fdao.getAll("FROM Admin");
+    }
+
+    @Override
+    public List<Object> search(String category, String search) {
+          return fdao.getAll("FROM Admin WHERE " + category + " LIKE '%" + search + "%'");
+    }
+
+    @Override
+    public Object getById(String id) {
+        return fdao.getById("from Admin where idAdmin='" + id + "'");
+    }
     
 }
+
